b74d67f9b600e2d97e95d8435406b8c605af3ee6
Krystian Baraniecki <254146@p.lodz.pl>
2024-03-25 20:41:32 +0100

    Get shop items by type in Presentation

--- ViewModel/ViewModel.cs
+++ ViewModel/ViewModel.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Windows.Input;
 using Model;
 
 namespace ViewModel
@@ -9,6 +12,20 @@ namespace ViewModel
     public class ViewModel : INotifyPropertyChanged
     {
         private Model.Model model;
+
+        private ObservableCollection<ItemPresentation> items;
+        public ObservableCollection<ItemPresentation> Items
+        {
+            get => items;
+            private set
+            {
+                if (items != value)
+                {
+                    items = value;
+                    OnPropertyChanged("Items");
+                }
+            }
+        }
 
         private string inflatcionString;
         public string InflationString
@@ -27,8 +44,14 @@ namespace ViewModel
         public ViewModel()
         {
             this.model = new Model.Model(null);
-
             model.InflationChanged += HandleInflationChanged;
+            Items = new ObservableCollection<ItemPresentation>(model.warehousePresentation.GetAvailableItems());
+
+            OnAllButtonCommand = new RelayCommand(() => HandleOnAllButton());
+            OnPotionsButtonCommand = new RelayCommand(() => HandleOnPotionsButton());
+            OnSwordsButtonCommand = new RelayCommand(() => HandleOnSwordsButton());
+            OnArmorsButtonCommand = new RelayCommand(() => HandleOnArmorsButton());
+            OnHelmetsButtonCommand = new RelayCommand(() => HandleOnHelmetsButton());
         }
 
         private void HandleInflationChanged(object sender, ModelInflationChangedEventArgs args)
@@ -36,10 +59,56 @@ namespace ViewModel
             InflationString = $"NewInflation: {args.NewInflation}";
         }
 
+        public ICommand OnAllButtonCommand { get; private set; }
+        private void HandleOnAllButton()
+        {
+            items.Clear();
+            model.warehousePresentation.GetAvailableItems().ToList().ForEach(items.Add);
+            PrintItems();
+        }
+
+        public ICommand OnPotionsButtonCommand { get; private set; }
+        private void HandleOnPotionsButton()
+        {
+            items.Clear();
+            model.warehousePresentation.GetItemsByType(PresentationItemType.Potion).ToList().ForEach(items.Add);
+            PrintItems();
+        }
+
+        public ICommand OnSwordsButtonCommand { get; private set; }
+        private void HandleOnSwordsButton()
+        {
+            items.Clear();
+            model.warehousePresentation.GetItemsByType(PresentationItemType.Sword).ToList().ForEach(items.Add);
+            PrintItems();
+        }
+
+        public ICommand OnArmorsButtonCommand { get; private set; }
+        private void HandleOnArmorsButton()
+        {
+            items.Clear();
+            model.warehousePresentation.GetItemsByType(PresentationItemType.Armor).ToList().ForEach(items.Add);
+            PrintItems();
+        }
+
+        public ICommand OnHelmetsButtonCommand { get; private set; }
+        private void HandleOnHelmetsButton()
+        {
+            items.Clear();
+            model.warehousePresentation.GetItemsByType(PresentationItemType.Helmet).ToList().ForEach(items.Add);
+            PrintItems();
+        }
+
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged([CallerMemberName] string? propertyName = null)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void PrintItems()
+        {
+            items.ToList().ForEach(item => Console.Out.WriteLine($"{item.Id}, {item.Name}, {item.Description}"));
+            Console.Out.WriteLine("-------");
         }
     }
 }
