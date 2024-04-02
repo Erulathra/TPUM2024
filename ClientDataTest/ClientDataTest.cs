d282d67167696406f5259751117191d17adb3046
Krystian Baraniecki <254146@p.lodz.pl>
2024-04-02 17:44:32 +0200

    Added client data tests

--- ClientDataTest/ClientDataTest.cs
+++ ClientDataTest/ClientDataTest.cs
@@ -1,11 +1,104 @@
+using ClientApi;
+using ClientData;
+using NuGet.Frameworks;
+
 namespace ClientDataTest;
 
 [TestClass]
 public class ClientDataTest
 {
-	[TestMethod]
-	public void TestMethod1()
+	static ConnectionServiceMock connectionService = new ConnectionServiceMock();
+	DataAbstractApi data = DataAbstractApi.Create(connectionService);
+
+	public void PrepareData()
 	{
-		// TODO: Krystian
+        connectionService.MockUpdateAll([
+			new ItemDTO(Guid.NewGuid(), "n1", "d", "Potion", 10.0f, false),
+            new ItemDTO(Guid.NewGuid(), "n2", "d", "Armor", 30.0f, false)
+		]);
+    }
+
+	[TestMethod]
+	public void UpdateAllTest()
+	{
+		ItemDTO[] itemDTOs = [
+			new ItemDTO(Guid.NewGuid(), "n1", "d", "Potion", 10.0f, false),
+			new ItemDTO(Guid.NewGuid(), "n2", "d", "Armor", 30.0f, false)
+		];
+
+		connectionService.MockUpdateAll(itemDTOs);
+		List<IItem> items = data.GetWarehouse().GetItems();
+
+        for (int i = 0; i < itemDTOs.Length; i++)
+        {
+			Assert.IsTrue(
+				itemDTOs[i].Id == items[i].Id
+				&& itemDTOs[i].Name == items[i].Name
+				&& itemDTOs[i].Description == items[i].Description
+				&& itemDTOs[i].Price == items[i].Price
+				&& itemDTOs[i].IsSold == items[i].IsSold);
+        }
+    }
+
+	[TestMethod]
+	public void InflationChangeTest()
+	{
+		PrepareData();
+
+		List<IItem> itemsBefore = data.GetWarehouse().GetItems();
+		float newInflation = 2.0f;
+		connectionService.MockInflationChanged(itemsBefore, 2.0f);
+		List<IItem> itemsAfter = data.GetWarehouse().GetItems();
+
+		for (int i = 0; i < itemsBefore.Count; i++)
+		{
+			Assert.AreEqual(itemsBefore[i].Price * newInflation, itemsAfter[i].Price);
+		}
 	}
+
+	[TestMethod]
+	public void SellItemTest()
+	{
+		Guid sellGuid = Guid.NewGuid();
+        connectionService.MockUpdateAll([
+			new ItemDTO(sellGuid, "n1", "d", "Potion", 10.0f, false),
+			new ItemDTO(Guid.NewGuid(), "n2", "d", "Armor", 30.0f, false)
+		]);
+
+		data.GetWarehouse().SellItem(sellGuid);
+
+		Assert.AreEqual(sellGuid, connectionService.lastSoldGuid);
+    }
+
+    [TestMethod]
+    public void GetItemsTest()
+    {
+		PrepareData();
+
+        Assert.AreNotEqual(data.GetWarehouse().GetItems().Count, 0);
+    }
+
+    [TestMethod]
+    public void GetItemByIdTest()
+    {
+		PrepareData();
+
+        List<IItem> items = data.GetWarehouse().GetItems();
+        IItem testItem = items[0];
+
+        Assert.AreEqual(testItem, data.GetWarehouse().GetItemByID(testItem.Id));
+    }
+
+    [TestMethod]
+    public void GetItemsByTypeTest()
+    {
+		PrepareData();
+
+        List<IItem> foundItems = data.GetWarehouse().GetItemsByType(ItemType.Potion);
+
+        foreach (IItem item in foundItems)
+        {
+            Assert.AreEqual(item.Type, ItemType.Potion);
+        }
+    }
 }
---
+++ ClientDataTest/ConnectionServiceMock.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientApi;
+using ClientData;
+
+namespace ClientDataTest
+{
+    internal class ConnectionServiceMock : IConnectionService
+    {
+        public event Action<string>? Logger;
+        public event Action? OnConnectionStateChanged;
+        public event Action<string>? OnMessage;
+        public event Action? OnError;
+        public event Action? OnDisconnect;
+
+        public Task Connect(Uri peerUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Disconnect()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsConnected()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task SendAsync(string message)
+        {
+            if (serializer.GetResponseHeader(message) == GetItemsCommand.StaticHeader)
+            {
+                UpdateAllResponse response = new UpdateAllResponse();
+                response.Items = [new ItemDTO(new Guid("testId"), "TestItem", "TestDesc", "Potion", 10.0f, false)];
+                OnMessage?.Invoke(serializer.Serialize(response));
+            }
+            else if (serializer.GetResponseHeader(message) == SellItemCommand.StaticHeader)
+            {
+                SellItemCommand sellItemCommand = serializer.Deserialize<SellItemCommand>(message);
+                lastSoldGuid = sellItemCommand.ItemID;
+
+                TransactionResponse response = new TransactionResponse();
+                response.Succeeded = true;
+                OnMessage?.Invoke(serializer.Serialize(response));
+            }
+
+            await new Task(() => { });
+        }
+
+        // Fields and methods for test purposes
+
+        private Serializer serializer = Serializer.Create();
+        public Guid lastSoldGuid;
+
+        public void MockInflationChanged(List<IItem> items, float newInflation)
+        {
+            InflationChangedResponse response = new InflationChangedResponse();
+            response.NewInflation = newInflation;
+
+            NewPriceDTO[] newPriceDTOs = new NewPriceDTO[items.Count];
+            int i = 0;
+            foreach (IItem item in items)
+            {
+                newPriceDTOs[i].ItemID = item.Id;
+                newPriceDTOs[i].NewPrice = item.Price * newInflation;
+                i++;
+            }
+            response.NewPrices = newPriceDTOs;
+
+            OnMessage?.Invoke(serializer.Serialize(response));
+        }
+
+        public void MockUpdateAll(ItemDTO[] items)
+        {
+            UpdateAllResponse response = new UpdateAllResponse();
+            response.Items = items;
+            OnMessage?.Invoke(serializer.Serialize(response));
+        }
+
+        /*        public void MockUpdateAllResponse()
+                {
+                    UpdateAllResponse = new UpdateAllResponse();
+                    JsonConvert.SerializeObject<>
+                    OnMessage?.Invoke();
+                }*/
+    }
+}
